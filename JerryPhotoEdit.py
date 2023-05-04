46ecc9ae6503e2ffdafba25a1c6ecde04d41dd83
TempAdv <TempAdv57@gmail.com>
2023-05-04 13:05:14 +0300

    Darken added

--- JerryPhotoEdit.py
+++ JerryPhotoEdit.py
@@ -21,7 +21,25 @@ def InvertImage(image):
             pixelO = tuple(pixelO)
             image.putpixel((x,y), pixelO)
 
-funs = {'Invert':InvertImage}
+def DarkenImage(image):
+    size = image.size
+    xs = size[0]; ys = size[1]
+    for y in range(ys):
+        
+        for x in range(xs):
+
+            #I - input O - output
+            pixelI = image.getpixel((x,y))
+            pixelO = []
+            pixelO.append(int(pixelI[0]**2/255))
+            pixelO.append(int(pixelI[1]**2/255))
+            pixelO.append(int(pixelI[2]**2/255))
+            pixelO.append(pixelI[3])
+            pixelO = tuple(pixelO)
+            image.putpixel((x,y), pixelO)
+
+funs = {'Invert':InvertImage,
+        'Darken':DarkenImage}
 
 print(files)
 
